826a000a43845c150033a6e1e001b2557a69d36e
App Academy Student <student@appacademy.io>
2013-04-06 15:27:51 -0700

    fiddle with static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,4 +31,19 @@ describe "StaticPages" do
     it { should have_selector('h1', text: 'Contact') }
     it { should have_selector('title', text: 'RoR Tut App | Contact') }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: 'About Us'
+    click_link "Help"
+    page.should have_selector 'title', text: 'Help'
+    click_link "Contact"
+    page.should have_selector 'title', text: 'Contact'
+    click_link "Home"
+    click_link "Sign up Now!"
+    page.should have_selector 'title', text: 'Sign Up'
+    click_link "sample-app"
+    page.should have_selector 'title', text: 'App'
+  end
 end

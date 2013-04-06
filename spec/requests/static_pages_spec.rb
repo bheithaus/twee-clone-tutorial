50df94ee84dcdf0d82e6a4ed70d6ae50c8e0fc7a
App Academy Student <student@appacademy.io>
2013-04-06 15:04:57 -0700

    finish up layout and routes, add UsersController

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have content 'Sample App'" do
-      visit "/static_pages/home"
-      page.should have_content('Sample App')
-    end
-
-    it "should have right title" do
-      visit "/static_pages/home"
-      page.should have_selector('title', text: "RoR Tut App")
-    end
-
-    it "should not have custom page title" do
-      visit "/static_pages/home"
-      page.should_not have_selector('title', text: "| Home")
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: "RoR Tut App") }
+    it { should_not have_selector('title', text: "| Home") }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have content 'Help'" do
-      visit "/static_pages/help"
-      page.should have_selector('h1', text: 'Help')
-    end
-
-    it "should have help title" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', text: 'Help')
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('h1', text: 'Help') }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have content 'About Us'" do
-      visit "/static_pages/about"
-      page.should have_selector('h1', text: 'About Us')
-    end
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: 'About Us') }
+  end
 
-    it "should have title 'About Us'" do
-      visit "/static_pages/about"
-      page.should have_selector('title', text: 'About Us')
-    end
+  describe "Contact Page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: 'RoR Tut App | Contact') }
   end
 end
